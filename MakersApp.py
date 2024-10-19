d713b872708e1c09841585caf539a5921debff83
LAROME2 <69281566+LAROME2@users.noreply.github.com>
2024-10-19 15:50:17 -0600

    py para crear db

--- MakersApp.py
+++ MakersApp.py
@@ -1,4 +1,4 @@
-from flask import (Flask, request, render_template)
+from flask import (Flask, request, render_template, redirect, session, make_response)
 import sqlite3
 
 
@@ -10,6 +10,19 @@ app.config['SECRET_KEY'] = 'SUPER SECRETO'
 @app.errorhandler(404)
 def not_Found(error):
     return render_template('404.html',error = error)
+
+@app.errorhandler(500)
+def not_found(error):
+    return render_template('500.html',error=error)
+
+#Ruta Principal
+@app.route ('/')
+def index():
+    user_ip = request.remote_addr
+    response = make_response(redirect('/welcome'))
+    session['user_ip'] = user_ip
+
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True)

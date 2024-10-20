05ceb04deefb935917c299596ddf42291d20e600
LAROME2 <69281566+LAROME2@users.noreply.github.com>
2024-10-19 19:05:47 -0600

    DB INTEGRATE

--- MakersApp.py
+++ MakersApp.py
@@ -1,11 +1,14 @@
 from flask import (Flask, request, render_template, redirect, session, make_response)
 import sqlite3
 
-
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'SUPER SECRETO'
 
+def get_db():
+    conn = sqlite3.connect('MakersData.db')
+    conn.row_factory = sqlite3.Row  # Permite acceder a los datos como diccionarios
+    return conn
 
 @app.errorhandler(404)
 def not_Found(error):
@@ -18,7 +21,11 @@ def not_found(error):
 #Ruta Principal
 @app.route ('/')
 def index():
-    return render_template('index.html')
+    conn = get_db()
+    productos = conn.execute('SELECT * FROM productos').fetchall()
+    ventas = conn.execute('SELECT * FROM ventas').fetchall()
+    conn.close()
+    return render_template('index.html', productos=productos, ventas=ventas)
 
 if __name__ == '__main__':
     app.run(debug=True)
